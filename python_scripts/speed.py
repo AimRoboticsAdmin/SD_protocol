a673b4fe8a1d1a9433e59d509e8d8fa83325eeab
AimBjarmi <bb@aim-robotics.com>
2025-03-20 07:16:02 -0700

    constraints for speed added to the protocol code

--- python_scripts/speed.py
+++ python_scripts/speed.py
@@ -4,8 +4,9 @@ from common import send_command
 import sys
 
 def set_dispensing_speed(speed):
-    command = f'speed {speed}'
-    send_command(command)
+    if speed >=0 and speed <=100:
+        command = f'speed {speed}'
+        send_command(command)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
--- python_scripts/tbps.py
+++ python_scripts/tbps.py
@@ -4,8 +4,9 @@ from common import send_command
 import sys
 
 def set_toolbar_speed(speed):
-    command = f'tbps {speed}'
-    send_command(command)
+    if speed >=0 and speed <=100:
+        command = f'tbps {speed}'
+        send_command(command)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
